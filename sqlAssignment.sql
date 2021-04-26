039edd10004c72f0e84d15201a5e6aa9fc962914
Shammi Kolonne <47134017+ShammiL@users.noreply.github.com>
2021-04-26 15:02:53 +0530

    Update sqlAssignment.sql

--- sqlAssignment.sql
+++ sqlAssignment.sql
@@ -3,44 +3,42 @@ CREATE DATABASE pets;
 use pets;
 
 CREATE TABLE owner (
-    owner_id INT PRIMARY KEY,
-    name VARCHAR(45),
-    surname VARCHAR(45),
-    street_address VARCHAR(60),
-    city VARCHAR(20),
-    state VARCHAR(20),
-    state_full VARCHAR(20),
-    zip_code INT
+	owner_id INT(5) PRIMARY KEY,
+    name VARCHAR(45) not null,
+    surname VARCHAR(45) not null,
+    street_address VARCHAR(60) not null,
+    city VARCHAR(20) not null,
+    state VARCHAR(20) not null,
+    state_full VARCHAR(20) not null,
+    zip_code INT(7) not null
 );
 
 CREATE TABLE pet (
     pet_id VARCHAR(100) PRIMARY KEY,
-    name VARCHAR(45),
-    kind VARCHAR(50),
-    gender VARCHAR(20),
-    age INT,
-    owner_id INT,
+    name VARCHAR(45) not null,
+    kind VARCHAR(50) not null,
+    gender VARCHAR(20) not null,
+    age INT(3) not null,
+    owner_id INT(5) not null,
     FOREIGN KEY (owner_id)
-        REFERENCES owner (owner_id)
+		REFERENCES owner (owner_id)
 );
 
 CREATE TABLE procedure_detail (
     procedure_type VARCHAR(50) NOT NULL,
-    procedure_subcode INT NOT NULL,
-    description VARCHAR(255),
-    price DECIMAL,
-    CONSTRAINT proc_detail PRIMARY KEY (procedure_type , procedure_subcode)
+    procedure_subcode INT(5) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (procedure_type , procedure_subcode)
 );
 
 CREATE TABLE procedure_history (
-    pet_id VARCHAR(100),
-    date DATE,
-    procedure_type VARCHAR(50),
-    procedure_subcode INT,
-    FOREIGN KEY (procedure_type)
-        REFERENCES procedure_detail (procedure_type),
-    -- FOREIGN KEY (procedure_subcode)
---         REFERENCES procedure_detail (procedure_subcode),
+    pet_id VARCHAR(100) NOT NULL,
+    date DATE NOT NULL,
+    procedure_type VARCHAR(50) NOT NULL,
+    procedure_subcode INT(5) NOT NULL,
+    FOREIGN KEY (procedure_type , procedure_subcode)
+        REFERENCES procedure_detail (procedure_type , procedure_subcode),
     FOREIGN KEY (pet_id)
         REFERENCES pet (pet_id)
 );
@@ -64,51 +62,42 @@ FROM
 WHERE
     price > 150;
     
-SELECT 
-    name
-FROM
-    owner
-WHERE
-    owner_id IN (SELECT 
-            owner_id
-        FROM
-            pet
-        WHERE
-            kind = 'parrot');
-
-SELECT 
-    COUNT(DISTINCT O.name)
-FROM
-    owner AS O,
-    owner AS N
-WHERE
-    O.zip_code = N.zip_code;
-    
-select date from procedure_history;
-
-SELECT 
-    name
+SELECT DISTINCT
+    owner.name
 FROM
     pet
+        JOIN
+    owner ON pet.owner_id = owner.owner_id
 WHERE
-    pet_id IN (SELECT 
-            pet_id
-        FROM
-            procedure_history
-        WHERE
-            MONTH(date) = 02 AND YEAR(date) = 2016);
+    kind = 'parrot';
+
+SELECT 
+    zip_code, COUNT(*)
+FROM
+    owner
+group by
+    zip_code;
+    
+SELECT DISTINCT
+    pet.name
+FROM
+    pet
+        JOIN
+    procedure_history ON pet.pet_id = procedure_history.pet_id
+WHERE
+    MONTH(date) = 02 AND YEAR(date) = 2016;
             
 SELECT 
     SUM(procedure_detail.price)
 FROM
-    (procedure_detail
-    INNER JOIN procedure_history ON procedure_detail.procedure_type = procedure_history.procedure_type
-        AND procedure_detail.procedure_subcode = procedure_history.procedure_subcode)
+    procedure_detail
+        JOIN
+    procedure_history ON procedure_detail.procedure_type = procedure_history.procedure_type
+        AND procedure_detail.procedure_subcode = procedure_history.procedure_subcode
+        JOIN
+    pet ON procedure_history.pet_id = pet.pet_id
+        JOIN
+    owner ON pet.owner_id = owner.owner_id
 WHERE
-    procedure_history.pet_id IN (SELECT 
-            pet.pet_id
-        FROM
-            (pet
-            INNER JOIN owner USING (owner_id))
-        WHERE
-            owner.zip_code = 49503);
+    owner.zip_code = 49503
+        AND MONTH(date) = 03;
